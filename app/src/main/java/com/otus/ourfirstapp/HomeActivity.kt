b74be6db983d78a2b5a9953403b2c4c461b50b0a
denis zhuravlev <denis.zhuravlev.it@gmail.com>
2020-07-20 20:46:00 +0200

    Initial commit

---
+++ app/src/main/java/com/otus/ourfirstapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.otus.ourfirstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //
+    }
+}
