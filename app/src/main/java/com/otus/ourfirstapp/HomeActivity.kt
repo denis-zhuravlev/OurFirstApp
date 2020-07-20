5ca4861b4954560aa8f89b925cc74d695482aafb
denis zhuravlev <denis.zhuravlev.it@gmail.com>
2020-07-20 20:49:53 +0200

    added new activity bla bla

--- app/src/main/java/com/otus/ourfirstapp/HomeActivity.kt
+++ app/src/main/java/com/otus/ourfirstapp/HomeActivity.kt
@@ -8,5 +8,9 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //
+        // master
+        // develop
+        //f sdfsd
+        ///fdfds
     }
 }
